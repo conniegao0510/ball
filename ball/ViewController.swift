fd42e68900e5c06648cdf78879951658ebb71c45
connie <409085260@qq.com>
2017-11-14 09:52:31 +0800

    20171114

--- ball/ViewController.swift
+++ ball/ViewController.swift
@@ -10,16 +10,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = .red
+        return cv
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        view.addSubview(collectionView)
+        //collectView.frame = view.frame
+        //use autolayout instead
+        collectionView.anchorToTop(top: view.topAnchor, left: view.leftAnchor,bottom: view.bottomAnchor, right: view.rightAnchor)
+        
     }
 
-    override func didReceiveMemoryWarning() {
+    /*override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }*/
+
+
+}
+
+extension UIView {
+    func anchorToTop(top: NSLayoutYAxisAnchor? = nil, left:
+        NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil, right:
+        NSLayoutXAxisAnchor? = nil){
+        anchorWithConstantsToTop(top: top, left: left, bottom:bottom, right:
+            right, topConstant:0, leftConstant:0,bottomConstant:0,
+                   rightConstant:0)
+        
     }
-
-
+    func anchorWithConstantsToTop(top: NSLayoutYAxisAnchor? = nil, left:
+        NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil,right:
+        NSLayoutXAxisAnchor? = nil, topConstant: CGFloat = 0, leftConstant:
+        CGFloat = 0, bottomConstant: CGFloat = 0, rightConstant: CGFloat = 0)
+        {
+        translatesAutoresizingMaskIntoConstraints = false
+            if let top = top {
+                topAnchor.constraint(equalTo: top, constant: topConstant).isActive = true
+            }
+            
+            if let bottom = bottom {
+                bottomAnchor.constraint(equalTo: bottom, constant: bottomConstant).isActive = true
+            }
+            
+            if let left = left {
+                leftAnchor.constraint(equalTo: left, constant: leftConstant).isActive = true
+            }
+            
+            if let right = right {
+                rightAnchor.constraint(equalTo: right, constant: rightConstant).isActive = true
+            }
+        }
 }
 
