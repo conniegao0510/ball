6b80abe29bc7ab877c5352125e10880527e37e48
connie <409085260@qq.com>
2017-11-16 08:53:41 +0800

    20171116

--- ball/ViewController.swift
+++ ball/ViewController.swift
@@ -9,62 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    let collectionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        cv.backgroundColor = .red
-        return cv
+    
+    let ballImageView: UIImageView = {
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "ball_64"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
     }()
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.addSubview(ballImageView)
+        //imageView.frame = CGRect(x: 50, y: 50, width: 60, height: 60)
         
-        view.addSubview(collectionView)
-        //collectView.frame = view.frame
-        //use autolayout instead
-        collectionView.anchorToTop(top: view.topAnchor, left: view.leftAnchor,bottom: view.bottomAnchor, right: view.rightAnchor)
+        setupLayout()
         
-    }
-
-    /*override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }*/
-
-
-}
-
-extension UIView {
-    func anchorToTop(top: NSLayoutYAxisAnchor? = nil, left:
-        NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil, right:
-        NSLayoutXAxisAnchor? = nil){
-        anchorWithConstantsToTop(top: top, left: left, bottom:bottom, right:
-            right, topConstant:0, leftConstant:0,bottomConstant:0,
-                   rightConstant:0)
-        
-    }
-    func anchorWithConstantsToTop(top: NSLayoutYAxisAnchor? = nil, left:
-        NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil,right:
-        NSLayoutXAxisAnchor? = nil, topConstant: CGFloat = 0, leftConstant:
-        CGFloat = 0, bottomConstant: CGFloat = 0, rightConstant: CGFloat = 0)
-        {
-        translatesAutoresizingMaskIntoConstraints = false
-            if let top = top {
-                topAnchor.constraint(equalTo: top, constant: topConstant).isActive = true
-            }
-            
-            if let bottom = bottom {
-                bottomAnchor.constraint(equalTo: bottom, constant: bottomConstant).isActive = true
-            }
-            
-            if let left = left {
-                leftAnchor.constraint(equalTo: left, constant: leftConstant).isActive = true
-            }
-            
-            if let right = right {
-                rightAnchor.constraint(equalTo: right, constant: rightConstant).isActive = true
-            }
         }
+    
+    private func setupLayout() {
+
+        // enables autolayout for our imageView
+        ballImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        //imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        ballImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 60).isActive = true
+        ballImageView.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        ballImageView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
+    }
+    
+    
 }
+
 
